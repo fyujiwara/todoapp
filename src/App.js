f8df7e377b2dffa798ac1507d7b2ba7222b0b5d0
yujiloh0816 <imp.yfujiwara@gmail.com>
2021-01-03 19:16:50 +0900

    Todoアイテムの更新処理を実装

--- src/App.js
+++ src/App.js
@@ -19,8 +19,19 @@ export class App {
             // それぞれのTodoItem要素をtodoListElement以下へ追加する
             const todoItems = this.todoListModel.getTodoItems();
             todoItems.forEach(item => {
-                const todoItemElement = element`<li>${item.title}</li>`;
+                const todoItemElement = item.completed 
+                    ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
+                    : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
                 todoListElement.appendChild(todoItemElement);
+                // チェックボックスがトグルしたときのイベントにリスナー関数を登録
+                const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+                inputCheckboxElement.addEventListener("change", () => {
+                    // 指定したTodoアイテムの完了状態を反転させる
+                    this.todoListModel.updateTodo({
+                        id: item.id,
+                        completed: !item.completed
+                    });
+                });
             });
             // containerElementの中身をtodoListElementで上書きする
             render(todoListElement, containerElement);

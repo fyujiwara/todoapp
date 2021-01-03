7a5865341d53400c26466ebe1e285eaf810d1841
yujiloh0816 <imp.yfujiwara@gmail.com>
2021-01-03 19:28:27 +0900

    Todoアイテムの削除機能を実装

--- src/App.js
+++ src/App.js
@@ -19,10 +19,16 @@ export class App {
             // それぞれのTodoItem要素をtodoListElement以下へ追加する
             const todoItems = this.todoListModel.getTodoItems();
             todoItems.forEach(item => {
-                const todoItemElement = item.completed 
-                    ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
-                    : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`;
-                todoListElement.appendChild(todoItemElement);
+                const todoItemElement = item.completed
+                ? element`<li><input type="checkbox" class="checkbox" checked>
+                    <s>${item.title}</s>
+                    <button class="delete">x</button>
+                </li>`
+                : element`<li><input type="checkbox" class="checkbox">
+                    ${item.title}
+                    <button class="delete">x</button>
+                </li>`;
+
                 // チェックボックスがトグルしたときのイベントにリスナー関数を登録
                 const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
                 inputCheckboxElement.addEventListener("change", () => {
@@ -32,6 +38,16 @@ export class App {
                         completed: !item.completed
                     });
                 });
+                
+                // 削除ボタン(x)がクリックされたときにTodoListModelからアイテムを削除する
+                const deleteButtonElement = todoItemElement.querySelector(".delete");
+                deleteButtonElement.addEventListener("click", () => {
+                    this.todoListModel.deleteTodo({
+                        id: item.id
+                    });
+                });
+
+                todoListElement.appendChild(todoItemElement);
             });
             // containerElementの中身をtodoListElementで上書きする
             render(todoListElement, containerElement);
